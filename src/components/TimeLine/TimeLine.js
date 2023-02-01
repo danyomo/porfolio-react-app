81a1893c68ee6a8dc073fd522c0c0ba84ca6cb9f
danyomo <59213891+danyomo@users.noreply.github.com>
2023-02-01 03:46:04 +0100

    Update TimeLine.js

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -4,7 +4,7 @@ import { CarouselButton, CarouselButtonDot, CarouselButtons, CarouselContainer, 
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { TimeLineData } from '../../constants/constants';
 
-/* const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
+ const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
 
 const Timeline = () => {
   const [activeItem, setActiveItem] = useState(0);
@@ -41,7 +41,7 @@ const Timeline = () => {
 
     window.addEventListener('resize', handleResize);
   }, []);
-  */
+  
   return (
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
