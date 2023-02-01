7107cb7c79b553e1197dee5ddaa5455ba23549b8
danyomo <59213891+danyomo@users.noreply.github.com>
2023-02-01 01:22:45 +0100

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,11 +9,11 @@ const Hero = (props) => (
     <Section row nopadding>
       <LeftSection>
         <SectionTitle main center>
-          Welcome To <br />
-          My Personal Portfolio
+          I am Daniel <br />
+          Welcome to my Portfolio
         </SectionTitle>
         <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+        Let me help you build that awesome app you love with my amazing skils, Am sure you would be impressed
         </SectionText>
         <Button onClick={props.handleClick}>Learn More</Button>
       </LeftSection>
