afe234edb933f52bc91bf045d7738fa86aa85b7f
danyomo <59213891+danyomo@users.noreply.github.com>
2023-02-06 05:40:42 +0100

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,12 +10,12 @@ export const projects = [
   },
  
    {
-   title: 'Gericht restuarant',
-    description: " Gericht Resturant app is the website that i implementes to improve my ReactJs skills. I use React hooks and other react best pratices to create this beautiful website",
-      image: '/images/1.PNG',
-      tags: ['Javascript', 'React'],
-    source: 'https://peppy-frangipane-9f0955.netlify.app/',
-    visit: ' https://github.com/danyomo/porfolio-react-app',
+   title: 'Travel Advisor React app',
+    description: " Travel Advisor React app is the website that I implemented to improve my ReactJs skills. I used React Hooks, Material UI and Google Map api to make it. You can search for the places you want to see the restaurant and hotel. It is fully responsive and works on any device. This website can be used to search for the places you want to see the restaurant and hotel. It is also a demo of my ReactJs skills. User can search the location and Type(Restaurant, Hotel). User can also filter the rating as well.",
+      image: '/images/2.PNG',
+      tags: ['React', 'Material UI','Google Map Api'],
+    source: 'https://dainty-kitsune-00325e.netlify.app/',
+    visit: ' https://github.com/danyomo/travel-advisor',
     id: 0,
   },
  
