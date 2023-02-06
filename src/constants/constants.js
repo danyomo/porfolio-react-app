255c1c78021e5c0005540c95f661d8c1727b4b40
danyomo <59213891+danyomo@users.noreply.github.com>
2023-02-06 05:27:30 +0100

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,6 +9,16 @@ export const projects = [
     id: 0,
   },
  
+   {
+   title: 'Gericht restuarant',
+    description: " Gericht Resturant app is the website that i implementes to improve my ReactJs skills. I use React hooks and other react best pratices to create this beautiful website",
+      image: '/images/1.PNG',
+      tags: ['Javascript', 'React'],
+    source: 'https://peppy-frangipane-9f0955.netlify.app/',
+    visit: ' https://github.com/danyomo/porfolio-react-app',
+    id: 0,
+  },
+ 
 ];
 
 export const TimeLineData = [
