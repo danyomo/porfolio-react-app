38891d0938498141b9a6331537a32725b6477f62
danyomo <59213891+danyomo@users.noreply.github.com>
2023-02-06 05:46:46 +0100

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -12,7 +12,7 @@ export const projects = [
    {
    title: 'Travel Advisor React app',
     description: " Travel Advisor React app is the website that I implemented to improve my ReactJs skills. I used React Hooks, Material UI and Google Map api to make it. You can search for the places you want to see the restaurant and hotel. It is fully responsive and works on any device. This website can be used to search for the places you want to see the restaurant and hotel. It is also a demo of my ReactJs skills. User can search the location and Type(Restaurant, Hotel). User can also filter the rating as well.",
-      image: '/images/2.PNG',
+      image: '/images/2.jpg',
       tags: ['React', 'Material UI','Google Map Api'],
     source: 'https://dainty-kitsune-00325e.netlify.app/',
     visit: ' https://github.com/danyomo/travel-advisor',
